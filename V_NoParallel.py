b974957b93f07ffa8ddb7739a1afebb9acaddcd5
Iulia-Andreea Ion <50475871+iuliaaion@users.noreply.github.com>
2022-04-10 10:47:03 +0300

    Add files via upload

---
+++ V_NoParallel.py
@@ -0,0 +1,109 @@
+def read_matrix(filename):
+    file = open(filename, 'r')
+    mat = []
+    m = 0
+    for line in file:
+        m = m + 1
+        n = 0
+        cells = line.strip()
+        row = []
+        for cell in cells:
+            if cell != ' ':
+                row.append(int(cell))
+                n = n + 1
+        mat.append(row)
+    return mat, m, n
+
+dict_obj = {0:"Jupiter",
+            1:"Mars",
+            2:"Mercury",
+            3:"Neptune",
+            4:"Pluto",
+            5:"Saturn",
+            6:"Earth",
+            7:"Uranus",
+            8:"Venus"}
+
+dict_atr = {0:"small",
+            1:"medium",
+            2:"large",
+            3:"near",
+            4:"far",
+            5:"yes",
+            6:"no"}
+
+I, m, n = read_matrix("small_planets.txt")
+X = [i for i in range(0, m)]
+Y = [i for i in range(0,n)]
+
+print(str(m) + " " + str(n))
+#for i in range(m):
+#    print(mat[i])
+#print(mat[1][1])
+
+rows = []
+for y in range(0, n):
+    objs = []
+    for x in range(0, m):
+        if I[x][y]:
+            objs.append(x)
+    objs_ind = [0 for _ in range(0, m)]
+    for i in range(0, len(objs)):
+        objs_ind[objs[i]] = 1
+    rows.append(objs_ind)
+    
+#print(rows)
+
+def intersection(P, Q):
+    indices =[]
+    for i in range(len(P)):
+        if P[i] == Q[i]:
+            indices.append(i)
+    return indices
+
+
+def computeClosure(A, B, y):
+    global I, m, n, rows
+    C = []
+    D = []
+    for i in range(m):
+        C.append(0)
+    for j in range(n):
+        D.append(1) 
+    for i in intersection(A, rows[y]):
+        C[i] = 1
+        for j in range(0, n):
+            if I[i][j] == 0:
+                D[j] = 0
+    return C, D
+
+def generateFrom(A, B, y):
+    global X, Y, I, m, n
+    print("A:")
+    print([dict_obj[i] for i in range(len(A)) if A[i] == 1])
+    print("B:")
+    print([dict_atr[i] for i in range(len(B)) if B[i] == 1])
+    if [i for i in range(len(B)) if B[i] == 1] == Y or y > n:
+        return
+    for j in range(y, n):
+        #print(j)
+        if B[j] == 0:
+            C, D = computeClosure(A, B, j)
+            skip = False
+            for k in range(0, j-1):
+                if D[k] != B[k]:
+                    skip = True
+                    break
+            if skip == False:
+                generateFrom(C, D, j+1)
+    return
+
+A = []
+B = []
+
+A = [1 for i in range(0, m)]
+B = [0 for _ in range(0, n)]
+
+generateFrom(A, B, 0)
+    
+    
